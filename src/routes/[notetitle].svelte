4bf9b8ef318e04d37c1d47c0a48a85100b449631
Ewan Nicolson <ewan.nicolson@gmail.com>
2021-01-23 16:44:18 +0000

    math rendering

--- src/routes/[notetitle].svelte
+++ src/routes/[notetitle].svelte
@@ -1,9 +1,13 @@
+<svelte:head>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
+
+</svelte:head>
+
 <script context="module">
     import hljs from "highlight.js"
+    import katex from "katex"
 	import { selectedNote } from './../stores.js';
     export async function preload(page, session){
-
-
 
         let { notetitle } = page.params;
 
@@ -14,7 +18,7 @@
 
         return {note}
     }
-</script>
+</script>  
 
 <script>
     import {onMount} from 'svelte';
@@ -28,6 +32,16 @@
             console.log(block)
 
         })
+    
+        var math = document.getElementsByClassName('math');
+        for (var i = 0; i < math.length; i++) {
+            const contents = math[i].textContent
+                .replace("\\[", "")
+                .replace("\\]", "")
+            //console.log(contents)
+            katex.render(contents, math[i]);
+        }
+
         $selectedNote = note.title
     })
 </script>

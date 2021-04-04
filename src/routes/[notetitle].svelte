29f7891e7347b8838671e3a1b80ff9070b88a074
Ewan Nicolson <ewan.nicolson@gmail.com>
2021-04-04 10:01:45 +0100

    Moving the note title out of the body and wrapping everything in an article.

--- src/routes/[notetitle].svelte
+++ src/routes/[notetitle].svelte
@@ -1,6 +1,9 @@
 <svelte:head>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css" integrity="sha384-AfEj0r4/OFrOo5t7NnNe46zW/tFgW6x/bCJG8FqQCEo3+Aro6EYUG4+cU+KJWu/X" crossorigin="anonymous">
 
+      <title>
+        {note.title}
+      </title>
 </svelte:head>
 
 <script context="module">
@@ -81,15 +84,13 @@
 </style>
 
 
-<h1>
-    {note.title}
-</h1>
-
-{#each sections as section}
-<div>
-    {@html section.sectionhtml}
-</div>    
-{/each}
+<article>
+  {#each sections as section}
+  <div>
+      {@html section.sectionhtml}
+  </div>    
+  {/each}
+</article>
 
 {#await linksPromise}
 <p>waiting</p>

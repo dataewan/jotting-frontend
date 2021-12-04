a070640669013d9a38f7768c1567214f62340dac
Ewan Nicolson <ewan.nicolson@gmail.com>
2021-12-04 09:57:40 +0000

    Localhost images path changed

--- src/images.js
+++ src/images.js
@@ -3,17 +3,10 @@ function lastPartURL(url) {
   return parts.pop() || parts.pop()
 }
 
-function firstPartURL(url) {
-  const parts = url.split("/")
-  return parts[0]
-}
-
 export function changeImageSource(image, server) {
   let originalSource = image.src
   let imagePath = lastPartURL(originalSource)
-  let firstPart = firstPartURL(originalSource)
-  console.log(firstPart)
-  if (!firstPart.includes("http")) {
+  if (originalSource.includes("localhost")) {
     image.src = `${server}/${imagePath}`
   }
   return image
